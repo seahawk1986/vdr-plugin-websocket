e8aa3efa554b5d257512bfc04ca7055f8d1b61b0
Alexander Grothe <seahawk1986@gmx.de>
2026-04-20 08:28:55 +0200

    Improve handling of strings if the system locale does not use UTF-8

--- common.hpp
+++ common.hpp
@@ -42,11 +42,21 @@ Debug(const char *Format, ...)
 
 inline std::string safeStr(const char *s)
 {
-    if (!s)
+    if (!s || !*s)
         return "";
 
+    // use a 'thread_local' converter to avoid reinstantiation
+    thread_local cCharSetConv conv(NULL, "UTF-8");
+
+    const char *source = conv.Convert(s);
+    if (!source)
+        source = s;
+
     std::string result;
-    const unsigned char *p = reinterpret_cast<const unsigned char *>(s);
+    // minimize reallocations for string concetenation
+    result.reserve(strlen(source));
+
+    const unsigned char *p = reinterpret_cast<const unsigned char *>(source);
 
     while (*p)
     {
@@ -54,51 +64,43 @@ inline std::string safeStr(const char *s)
 
         if (c < 0x80)
         {
-            // ASCII & Steuerzeichen (Tab, LF, etc.) einfach übernehmen
             result += static_cast<char>(c);
             p++;
         }
         else if ((c & 0xE0) == 0xC0)
-        { // 2-Byte UTF-8
-            if ((p[1] & 0xC0) == 0x80)
+        { // 2-Byte
+            if (p[1] && (p[1] & 0xC0) == 0x80)
             {
                 result.append(reinterpret_cast<const char *>(p), 2);
                 p += 2;
             }
             else
-            {
                 goto replace;
-            }
         }
         else if ((c & 0xF0) == 0xE0)
-        { // 3-Byte UTF-8
-            if ((p[1] & 0xC0) == 0x80 && (p[2] & 0xC0) == 0x80)
+        { // 3-Byte
+            if (p[1] && (p[1] & 0xC0) == 0x80 && p[2] && (p[2] & 0xC0) == 0x80)
             {
                 result.append(reinterpret_cast<const char *>(p), 3);
                 p += 3;
             }
             else
-            {
                 goto replace;
-            }
         }
         else if ((c & 0xF8) == 0xF0)
-        { // 4-Byte UTF-8
-            if ((p[1] & 0xC0) == 0x80 && (p[2] & 0xC0) == 0x80 && (p[3] & 0xC0) == 0x80)
+        { // 4-Byte
+            if (p[1] && (p[1] & 0xC0) == 0x80 && p[2] && (p[2] & 0xC0) == 0x80 && p[3] && (p[3] & 0xC0) == 0x80)
             {
                 result.append(reinterpret_cast<const char *>(p), 4);
                 p += 4;
             }
             else
-            {
                 goto replace;
-            }
         }
         else
         {
         replace:
-            // Ungültiges Byte (wie dein 0x80) durch Unicode-Replacement-Char (UTF-8) ersetzen
-            result += "\xEF\xBF\xBD";
+            result += "\xEF\xBF\xBD"; // Unicode Replacement Character
             p++;
         }
     }
